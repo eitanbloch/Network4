4ed29b0b733cb7cfd4aa558958ce1bfdddf0adf5
Eitan Bloch <eitan.a.bloch@gmail.com>
2023-07-02 19:45:06 +0300

    v36

--- lb.cpp
+++ lb.cpp
@@ -356,6 +356,9 @@ void handle_server(int server_id) {
     if (server.id == 1){
         cout << "server 1: is_busy: " << server.is_busy << endl;
     }
+    if (server.id != 1 && server_id == 1){
+        cout << "wanted id 1 but real id is: " << server.id << endl;
+    }
     if (server.is_busy || task_list.empty())
         return;
     /*
